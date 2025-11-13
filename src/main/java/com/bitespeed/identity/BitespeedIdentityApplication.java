6d2cbd837acdfc6b5ee765425cb9dbef5dd26a1e
Yashas M <yashasm@Yashass-MacBook-Air.local>
2025-11-13 22:13:24 +0530

    Initial commit - project structure

---
+++ src/main/java/com/bitespeed/identity/BitespeedIdentityApplication.java
@@ -0,0 +1,12 @@
+package com.bitespeed.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BitespeedIdentityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BitespeedIdentityApplication.class, args);
+	}
+}
